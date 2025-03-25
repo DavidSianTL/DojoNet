2a8c48d3055d49c3ffe672f55fe16a26d80b4162
davidsianTL <dsian@digitalgeko.com>
2025-03-25 16:23:16 -0600

    se agrego el proyecto LandingPage

---
+++ Ejemplos/LandingPageCV/LandingPageCV/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LandingPageCV.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
