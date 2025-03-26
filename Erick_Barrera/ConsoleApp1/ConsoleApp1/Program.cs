e48acd6caa463b0ba9350a111da0dffcce43bd48
Erickbs21 <ebarrera@digitalgeko.com>
2025-03-26 17:19:31 -0600

    Mi primer c#

---
+++ Erick_Barrera/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        
+        Console.Write("Ingrese su nombre: ");
+        string nombre = Console.ReadLine();
+
+        
+        Console.Write("Ingrese su edad: ");
+        int edad = int.Parse(Console.ReadLine());
+
+        
+        string mensajeEdad = (edad >= 18) ? "Eres mayor de edad." : "Eres menor de edad.";
+
+        
+        Console.Write("Ingrese su calificación: ");
+        int calificacion = int.Parse(Console.ReadLine());
+
+       
+        string mensajeCalificacion = (calificacion >= 60) ? "Felicidades, has aprobado." : "Lo siento, has reprobado.";
+
+        
+        Console.WriteLine($"\n{mensajeEdad}");
+        Console.WriteLine(mensajeCalificacion);
+        Console.WriteLine($"Gracias por usar nuestro programa, {nombre}.");
+    }
+}
+
