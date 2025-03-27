7256abcc57f6897279cbc2340fd4c0f16afcaf37
Erickbs21 <ebarrera@digitalgeko.com>
2025-03-27 14:04:17 -0600

    Agregada la carpeta Retos a la rama Erick_Barrera

---
+++ Retos/Tipos de datos.cs
@@ -0,0 +1,10 @@
+using System; 
+
+//*Declara tres variables:
+//Un número entero
+//Un texto
+//Un valor booleano
+
+int Numero = 10; 
+int Texto = "Hola jajss;
+bool Esfalso = false; 
---
+++ Retos/Variables.cs
@@ -0,0 +1,7 @@
+using System; 
+
+//Ejemplo de variables 
+
+string Nombre = "Erick"; 
+int edad = 21; 
+
