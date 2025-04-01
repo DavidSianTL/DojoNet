42848d48495731aede71edd362399d6b6e463351
Estiben-Hernandez <jhernandez@digitalgeko.com>
2025-04-01 08:26:41 -0600

    agregar vista,modelo,controlador

--- Prueba1-Junior/MVC/Geko-MVC-Project/Geko-MVC-Project/Controllers/HomeController.cs
+++ Prueba1-Junior/MVC/Geko-MVC-Project/Geko-MVC-Project/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 using System.Diagnostics;
+using System.Reflection.Metadata.Ecma335;
 using Geko_MVC_Project.Models;
 using Microsoft.AspNetCore.Mvc;
 
@@ -13,10 +14,40 @@ namespace Geko_MVC_Project.Controllers
             _logger = logger;
         }
 
-        public IActionResult Index()
+        public IActionResult Index(UsuarioModel usuarioModel)
+
         {
-            return View();
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    ViewBag.Mensaje = "exito";
+                    return View("index", usuarioModel);
+                }
+                else
+                {
+                    ViewBag.Mensaje = "malo"; }
+                return View("index", usuarioModel|);
+                }
+            }
+            catch
+            {
+                logger.LogError(Exception, Exception.Message)
+                ViewBag.Mensaje = "Ocurrio un error inesperado";
+                Return view("index", usuarioModel);
         }
+        }
+
+
+
+        [HttpPost]
+        public IActionResult Registro()
+        {
+
+        }
+
+
+
 
         public IActionResult Privacy()
         {
--- Prueba1-Junior/MVC/Geko-MVC-Project/Geko-MVC-Project/Models/UsuarioModel.cs
+++ Prueba1-Junior/MVC/Geko-MVC-Project/Geko-MVC-Project/Models/UsuarioModel.cs
@@ -4,14 +4,9 @@ namespace Geko_MVC_Project.Models
 {
     public class UsuarioModel
     {
-        public int Id { get; set; }
-        public string strNombre { get; set; }
-        public string strApellido { get; set; }
-        public string Username { get; set; }
-        public string Puesto { get; set; }  
-        public string FechaIngreso { get; set; }
-        public string Status{ get; set; }
-        public string roll { get; set; }
-
+        
+        public string Nombre { get; set; }
+        public int Email { get; set; }
+        public string password { get; set; }
     }
 }
