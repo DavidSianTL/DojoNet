62aa9c09089b914ea02a7413e10b3cac3b029ffa
Daniel Roblero <droblero@digitalgeko.com>
2025-04-02 07:02:43 -0600

    practica web

---
+++ daniel_roblero/practicaWeb/pruebaWeb/pruebaWeb/Controllers/EmpleadoController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using pruebaWeb.Models; 
+using System.Collections.Generic;
+using System.Linq;
+
+namespace pruebaWeb.Controllers
+{
+    public class EmpleadoController : Controller
+    {
+        //simularemos una base de datos con listas de empleados
+        private static List<EmpleadoModelo> empleados = new List<EmpleadoModelo>();
+        public IActionResult Crear()
+        {
+            return View(empleados);
+        }
+        //Guardar empleados
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public IActionResult Crear(EmpleadoModelo empleado)
+        {
+            if (ModelState.IsValid)
+            {
+                empleados.Add(new Empleado
+                {
+                    //Agregar empleados a la lista
+                    empleado.Id, = empleados.Count + 1,
+                    empleados.Add(empleado);
+                    return RedirectToAction(nameof(Index);
+            });
+                return RedirectToAction("Index");
+            }
+            return View(empleados);
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ daniel_roblero/practicaWeb/pruebaWeb/pruebaWeb/Models/EmpleadoModelo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace pruebaWeb.Models
+{
+    public class EmpleadoModelo
+    {
+        public int Id { get; set; }
+            
+        [Required]
+        [StringLength(50)]
+        public string Nombre { get; set; }
+        [Required]
+        [StringLength(50)]
+        [Range(0, double.MaxValue, ErrorMessage = "El salario debe ser un numero postivo")]
+
+        public string Puesto { get; set; }
+
+        public decimal Salario { get; set; }
+        [Required]
+        [DataType(DataType.Date)]
+
+        public DateTime FechaContratacion { get; set; }
+    }
+}
