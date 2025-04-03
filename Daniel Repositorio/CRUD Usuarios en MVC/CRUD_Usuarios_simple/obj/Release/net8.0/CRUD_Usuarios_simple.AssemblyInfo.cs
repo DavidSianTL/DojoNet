a9ad6153810f7c17f9f81d1d94efa65eaf3b71c2
Daniel Roblero <droblero@digitalgeko.com>
2025-04-02 21:54:17 -0600

    Agregacion de referencia botones

--- Daniel Repositorio/CRUD Usuarios en MVC/CRUD_Usuarios_simple/obj/Release/net8.0/CRUD_Usuarios_simple.AssemblyInfo.cs
+++ Daniel Repositorio/CRUD Usuarios en MVC/CRUD_Usuarios_simple/obj/Release/net8.0/CRUD_Usuarios_simple.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("CRUD_Usuarios_simple")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de3c07c2324536e21f80e4e987c2167eafcb9b87")]
 [assembly: System.Reflection.AssemblyProductAttribute("CRUD_Usuarios_simple")]
