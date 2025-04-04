194dc98c4752e2e31cf80a043140d6821ee68e4b
Pablo torres <ptorres@digitalgeko.com>
2025-04-04 15:28:58 -0600

    hola proyectos

---
+++ Proyectos de Ejemplo/CRUD Usuarios en MVC/CRUD_Usuarios_simple/Controllers/EditarController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CRUD_Usuarios_simple.Controllers
+{
+    public class EditarController : Controller
+    {
+        public IActionResult Editar()
+        {
+            return View();
+        }
+    }
+}
---
+++ Proyectos de Ejemplo/Ejercios en consola C#/TryCatch/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+//Ejemplo de try catch
+class Program
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            Console.WriteLine("Introduce un número:");
+            int numero = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("El número es: " + numero);
+        }
+        catch (FormatException e)
+        {
+            Console.WriteLine("Error: Formato de número inválido. " + e.Message);
+        }
+        catch (OverflowException e)
+        {
+            Console.WriteLine("Error: El número es demasiado grande o pequeño. " + e.Message);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Error inesperado: " + e.Message);
+        }
+    }
+}
