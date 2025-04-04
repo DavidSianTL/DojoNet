975349698b656f8a1c568ec20a778b991ce11674
PeGhoste <jopalvarado1@gmail.com>
2025-04-04 12:47:50 -0600

    Trayendo la carpeta (Proyectos de Ejemplo) desde main

---
+++ Proyectos de Ejemplo/CRUD Usuarios en MVC/CRUD_Usuarios_simple/Controllers/EstadoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CRUD_Usuarios_simple.Controllers
+{
+    public class EstadoController : Controller
+    {
+        public IActionResult Estado()
+        {
+            return View();
+        }
+    }
+}
