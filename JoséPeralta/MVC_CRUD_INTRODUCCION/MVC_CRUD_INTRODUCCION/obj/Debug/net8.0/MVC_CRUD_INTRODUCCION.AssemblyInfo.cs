7bcc09ac47398ed5cc165503b2e71202c3da6aa0
PeGhoste <jopalvarado1@gmail.com>
2025-04-04 19:08:39 -0600

    Nuevo proyecto de Angular con .NET

--- JoséPeralta/MVC_CRUD_INTRODUCCION/MVC_CRUD_INTRODUCCION/obj/Debug/net8.0/MVC_CRUD_INTRODUCCION.AssemblyInfo.cs
+++ JoséPeralta/MVC_CRUD_INTRODUCCION/MVC_CRUD_INTRODUCCION/obj/Debug/net8.0/MVC_CRUD_INTRODUCCION.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MVC_CRUD_INTRODUCCION")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c3e15a2b97b2a3552492c9bbd6d86315ebc96d2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+975349698b656f8a1c568ec20a778b991ce11674")]
 [assembly: System.Reflection.AssemblyProductAttribute("MVC_CRUD_INTRODUCCION")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MVC_CRUD_INTRODUCCION")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
