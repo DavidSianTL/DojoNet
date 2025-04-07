e060d3d35dcfcb72d0b908a71450dd78a21d4dc0
Daniel Roblero <droblero@digitalgeko.com>
2025-04-07 13:20:35 -0600

    Implementacion de proyecto script

---
+++ Daniel Repositorio/MiniReto_javaScript/MiniReto_javaScript/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MiniReto_javaScript.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
