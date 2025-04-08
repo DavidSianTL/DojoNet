88ffdfeab556013e2b6958253fa1639d59f1dea1
Carlos <ccaal@digitalgeko.com>
2025-04-07 22:55:37 -0600

    Subiendo Reto JAVA

---
+++ RetoJavaScript/RetoJavaScript/Models/Tienda.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RetoJavaScript.Models
+{
+    public class Tienda
+
+    {
+        [Required]
+        [RegularExpression("^[a-zA-ZáéíóúÁÉÍÓÚñÑ ]+$")]
+        public string? Producto { get; set; }
+
+        [Required]
+        [Range(0.01, double.MaxValue)]
+        public double? Precio { get; set; }
+
+        [Required]
+        [Range(1, int.MaxValue)]
+        public int? Id { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime? FechaDeVencimiento { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string? correo { get; set; }
+        [Required]
+        public int? CategoriaId { get; set; }
+
+    }
+}
+
