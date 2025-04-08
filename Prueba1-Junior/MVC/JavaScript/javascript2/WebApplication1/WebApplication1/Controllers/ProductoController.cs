b41bfce2a88ea3d521b5e48faf73b20bfc289d46
Estiben-Hernandez <jhernandez@digitalgeko.com>
2025-04-08 08:16:57 -0600

    Add: agregar proyecto pruebaJs2

---
+++ Prueba1-Junior/MVC/JavaScript/javascript2/WebApplication1/WebApplication1/Controllers/ProductoController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using pruebaJs2.Models;
+
+namespace pruebaJs2.Controllers;
+
+public class ProductoController : Controller
+{
+    public IActionResult Crear()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Crear(Producto producto)
+    {
+        return View("Confirmacion", producto);
+    }
+}
---
+++ Prueba1-Junior/MVC/JavaScript/javascript2/WebApplication1/WebApplication1/Models/ProductoModel.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Authentication;
+
+namespace pruebaJs2.Models
+{
+    public class Producto
+    {
+        public int id { get; set; }
+        public string Nombre { get; set; }
+        public decimal Precio { get; set; }
+        public string Categoria { get; set; }
+    }
+}
