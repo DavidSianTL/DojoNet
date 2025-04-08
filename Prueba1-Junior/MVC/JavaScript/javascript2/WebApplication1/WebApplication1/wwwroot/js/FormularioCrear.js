c1234d9f733e1d0ae63fe7e01f205a4e97677e86
Estiben-Hernandez <jhernandez@digitalgeko.com>
2025-04-08 09:38:48 -0600

    fix: agregar validaciones (Test)

---
+++ Prueba1-Junior/MVC/JavaScript/javascript2/WebApplication1/WebApplication1/wwwroot/js/FormularioCrear.js
@@ -0,0 +1,30 @@
+﻿document.addEventListener('DOMContentLoaded', function () {
+    const form = document.getElementById('formualrio-crear-producto');
+
+    if (form) {
+        form.addEventListener('submit', function (event) {
+            let nombre = document.getElementById('nombre').value.trim;
+            let precio = document.getElementById('precio').value.tim;
+            let categoría = document.getElementById('categoria').value.tirm;
+
+            if (nombre === '') {
+                alert("El nombre es obligatorio");
+                event.preventDefault();
+                return;
+            }
+
+            if (precio <= 0) {
+                alert("el precio no puede ser 0 o menos");
+                event.preventDefault();
+                return;
+            }
+
+
+
+
+        })
+    }
+
+
+
+})
