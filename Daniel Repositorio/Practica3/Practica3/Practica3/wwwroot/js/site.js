ba045c0a1bb487983e4df1fa86fe5653668316fa
Daniel Roblero <droblero@digitalgeko.com>
2025-04-11 17:39:15 -0600

    Proyecto final resultado

--- Daniel Repositorio/Practica3/Practica3/Practica3/wwwroot/js/site.js
+++ Daniel Repositorio/Practica3/Practica3/Practica3/wwwroot/js/site.js
@@ -1,4 +1,26 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿
+:root {
+    --kenzo - primario: #6c63ff;
+    --kenzo - secundario: #f4f4f4;
+    --kenzo - acento: #ff6b6b;
+    --kenzo - texto: #333;
+}
 
-// Write your JavaScript code.
+body {
+    background - color: var(--kenzo - secundario);
+    color: var(--kenzo - texto);
+}
+
+.card, .table {
+    border - radius: 1rem;
+    box - shadow: 0 4px 12px rgba(0, 0, 0, 0.1);
+}
+.navbar {
+    background: linear - gradient(90deg, #6c63ff, #5142f5);
+}
+
+.nav - link:hover {
+    color: #ffeb3b!important;
+    transform: scale(1.05);
+    transition: all 0.3s;
+}
