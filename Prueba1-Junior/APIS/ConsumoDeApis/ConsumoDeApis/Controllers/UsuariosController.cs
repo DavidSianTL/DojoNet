40bda1c4deac2d98736c70c0c7d38b9eb4e0efe8
Estiben-Hernandez <jhernandez@digitalgeko.com>
2025-04-21 16:03:45 -0600

    add: agregar carpeta APIS & proyecto ConsumoDeApis

---
+++ Prueba1-Junior/APIS/ConsumoDeApis/ConsumoDeApis/Controllers/UsuariosController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ConsumoDeApis.Controllers
+{
+    public class UsuariosController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
