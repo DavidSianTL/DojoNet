2f7a0bd5a743057cac4ca86a4c73f4c58e261563
Diego Catalan <dcatalan@digitalgeko.com>
2025-04-21 15:20:07 -0600

    Consumo de api

---
+++ Diego C/Consumir Api/Consumir Api/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+using System.Diagnostics;
+using Consumir_Api.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Consumir_Api.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult ApiRickAndMorty()
+        {
+            return View();
+        }
+        public IActionResult vivos()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Diego C/Consumir Api/Consumir Api/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Consumir_Api.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
