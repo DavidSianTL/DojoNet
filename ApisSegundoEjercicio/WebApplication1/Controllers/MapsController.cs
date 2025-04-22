b7df9287ea52e499ba3517bca85ecbe04abcf085
Carlos <ccaal@digitalgeko.com>
2025-04-21 21:48:34 -0600

    Mini Reto haciendo uso de javascript para consumir api

---
+++ ApisSegundoEjercicio/WebApplication1/Controllers/MapsController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class MapsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
+
---
+++ ApisSegundoEjercicio/WebApplication1/Controllers/WeaponsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TuProyecto.Controllers
+{
+    public class WeaponsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
