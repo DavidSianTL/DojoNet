caa72dadbd3aebab4622badf9f6c893ae6240e6e
PeGhoste <jopalvarado1@gmail.com>
2025-04-23 10:07:48 -0600

    Guardando cambios o posibles conflictos en los ejemplos de las API

---
+++ JoséPeralta/EjemploApiSoap/EjemploApiSoap/Services/CountryInfo.cs
@@ -0,0 +1,51 @@
+﻿using CountryInfoService;
+
+namespace EjemploApiSoap.Services
+{
+    public interface ICountryInfo
+    {
+        Task<string> CountryInfoService(string country); 
+    }
+
+    public class CountryInfo : ICountryInfo
+    {
+        public async Task<string> CountryInfoService(string country)
+        {
+            try
+            {
+                // Validación de entrada
+                if (string.IsNullOrEmpty(country))
+                {
+                    File.WriteAllText("log.txt", "El país no puede ser nulo.");
+                    throw new ArgumentNullException(nameof(country), "El país no puede ser nulo.");
+                }
+
+                // Conexión a servicio SOAP
+                var cliente = new CountryInfoServiceSoapTypeClient(CountryInfoServiceSoapTypeClient.EndpointConfiguration.CountryInfoServiceSoap);
+
+                // Llamada al servicio SOAP
+                var resultado = await cliente.CapitalCityAsync(country);
+
+                // Verificamos que el resultado no sea nulo
+                if (resultado == null)
+                {
+                    File.WriteAllText("log.txt", "No se pudo obtener la capital de Guatemala.");
+                    throw new Exception("No se pudo obtener la capital de Guatemala.");
+                }
+
+                // Cierre de la conexión
+                await cliente.CloseAsync();
+
+                // Retorno del resultado
+                return resultado.Body.CapitalCityResult;
+            }
+            catch (Exception e)
+            {
+                // Manejo de excepciones
+                System.IO.File.WriteAllText("log.txt", e.ToString());
+                return "Error al consultar la Capital: " + e.Message;
+            }
+        }
+    }
+
+}
---
+++ JoséPeralta/EjemploConsumirApiRest/EjemploConsumirApiRest/Models/ServiceViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace EjemploConsumirApiRest.Models
+{
+    public class ServiceViewModel
+    {
+        [JsonPropertyName("id")]
+        [Required]
+        public string Id { get; set; }
+
+        [JsonPropertyName("name")]
+        [Required(ErrorMessage = "El nombre del producto no puede faltar")]
+        [StringLength(100, ErrorMessage = "El nombre del producto no puede tener más de 50 caracteres")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("data")]
+        public Dictionary<string, JsonElement> Data { get; set; }
+    }
+}
