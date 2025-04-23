6de8060df4827d475cbefceb9feb981295aac5bd
Anika Escoto <aescoto@digitalgeko.com>
2025-04-23 14:01:24 -0600

    Cambios minimos en el estilo

--- ConsumoServiciosSoap/ConsumoServiciosSoap/obj/Debug/net8.0/ConsumoServiciosSoap.AssemblyInfo.cs
+++ ConsumoServiciosSoap/ConsumoServiciosSoap/obj/Debug/net8.0/ConsumoServiciosSoap.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsumoServiciosSoap")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+91db55ddc3e90ea09dfd99f27d5ddd067dfec82f")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsumoServiciosSoap")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsumoServiciosSoap")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
