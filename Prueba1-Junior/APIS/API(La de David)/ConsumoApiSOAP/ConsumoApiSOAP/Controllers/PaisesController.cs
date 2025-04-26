850fd7ccbdae7f98a80e4cdf96b40be45b12ee54
Estiben-Hernandez <jhernandez@digitalgeko.com>
2025-04-25 19:54:55 -0600

    add: agregar proyecto ConsumoDeApiSOAP +feature

---
+++ Prueba1-Junior/APIS/API(La de David)/ConsumoApiSOAP/ConsumoApiSOAP/Controllers/PaisesController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ConsumoApiSOAP.Services;
+
+namespace ConsumoApiSOAP.Controllers
+{
+    public class PaisesController : Controller
+    {
+        private readonly ICountryInfoService _countryInfoService;
+
+        public PaisesController(ICountryInfoService countryInfoService)
+        {
+            _countryInfoService = countryInfoService;
+
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult PaisPorCodigo()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> PaisPorCodigo(string codigoISO)
+        {
+            string pais = string.Empty;
+            
+            pais =  await _countryInfoService.paisPorCodigoAsync(codigoISO);
+
+            ViewBag.Pais = pais;
+            return View();
+        }
+    }
+
+}
---
+++ Prueba1-Junior/APIS/API(La de David)/ConsumoApiSOAP/ConsumoApiSOAP/Services/ServicioCountryInfo.cs
@@ -0,0 +1,48 @@
+﻿using CountryInfoService;
+namespace ConsumoApiSOAP.Services
+{
+
+	public interface ICountryInfoService
+	{
+		public Task<string> paisPorCodigoAsync(string codigoISO);
+
+        public Task<string> capitalDePaisPorCodigoAsync(string codigoISO);
+	}
+
+    public class ServicioCountryInfo : ICountryInfoService
+	{
+
+		private CountryInfoServiceSoapTypeClient crearCliente()
+		{
+			return new CountryInfoServiceSoapTypeClient(
+				CountryInfoServiceSoapTypeClient.EndpointConfiguration.CountryInfoServiceSoap
+			);
+			
+		}
+        
+
+		public async Task<string> paisPorCodigoAsync(string codigoISO)
+		{
+			var cliente = crearCliente();
+
+			var content = await cliente.CountryNameAsync(codigoISO);
+
+			await cliente.CloseAsync();
+
+			return content.Body.CountryNameResult;
+        }
+
+        public async Task<string> capitalDePaisPorCodigoAsync(string codigoISO)
+		{
+			var cliente = crearCliente();
+
+            var content = await cliente.CapitalCityAsync(codigoISO);
+
+			await cliente.CloseAsync();
+
+
+			return content.Body.CapitalCityResult;
+        }
+
+    }
+}
