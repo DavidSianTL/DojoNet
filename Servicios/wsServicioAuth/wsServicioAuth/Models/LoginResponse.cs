24b15fc337e247e95a1f49cefe759bf91d30846e
David Sian LT <dsian@digitalgeko.com>
2025-04-28 14:44:40 -0600

    cambios antes del pull

---
+++ Servicios/wsServicioAuth/wsServicioAuth/Models/LoginResponse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace wsServicioAuth.Models
+{
+    public class LoginResponse
+    {
+        public bool Success { get; set; }
+        public string Message { get; set; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+
+    }
+}
