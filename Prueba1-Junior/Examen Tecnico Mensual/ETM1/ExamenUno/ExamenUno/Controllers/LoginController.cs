0b07ba7a8e5a24ff4331a8b8204cbabaf600af15
Estiben-Hernandez <jhernandez@digitalgeko.com>
2025-04-28 16:23:39 -0600

    feature: agregar Login

---
+++ Prueba1-Junior/Examen Tecnico Mensual/ETM1/ExamenUno/ExamenUno/Controllers/LoginController.cs
@@ -0,0 +1,34 @@
+﻿using System.Text.Json;
+using ExamenUno.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExamenUno.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Login(Usuario user)
+        {
+            var rutaUsuarios = Path.Combine(Directory.GetCurrentDirectory(), "Data", "Usuarios.json");
+            var content = System.IO.File.ReadAllText(rutaUsuarios);
+            var users = JsonSerializer.Deserialize<List<Usuario>>(content);
+
+            var validUser = users.FirstOrDefault(u=>
+                u.username == user.username &&
+                u.password == user.password
+            );
+
+            if(validUser != null)
+            {
+                HttpContext.Session.SetString("User", validUser.username);
+                return RedirectToAction("Index", "Home");
+            }
+
+            return View();
+        }
+    }
+}
