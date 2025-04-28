98451f3439c6111cf3ff6d03ae940ea7ae37b38e
Estiben-Hernandez <jhernandez@digitalgeko.com>
2025-04-28 15:58:02 -0600

    add: Agregar directorio de Examen Tecnico Mensual/ETM1/ & proyecto: 'ExamenUno'

---
+++ Prueba1-Junior/Examen Tecnico Mensual/ETM1/ExamenUno/ExamenUno/Models/Usuario.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ExamenUno.Models
+{
+    public class Usuario
+    {
+        [Required]
+        public string username { get; set; } = string.Empty;
+        [Required]
+        public string password {  get; set; } = string.Empty;
+    }
+}
