42b832b3861d3bf9c4e421e36a5c04218b9ca9e8
Daniel Roblero <droblero@digitalgeko.com>
2025-04-30 17:26:54 -0600

    Arreglando letras del producto como colores

---
+++ Daniel Repositorio/Daniel_Evaluacion_Mensual_Abril/ExamDaniel/publish/wwwroot/js/sweetAlert.js
@@ -0,0 +1,18 @@
+﻿function mostrarSweetAlertExito(mensaje) {
+    Swal.fire({
+        icon: 'success',
+        title: 'Éxito',
+        text: mensaje,
+        confirmButtonColor: '#198754'
+    });
+}
+
+function mostrarSweetAlertError(mensaje) {
+    Swal.fire({
+        icon: 'error',
+        title: 'Error',
+        text: mensaje,
+        confirmButtonColor: '#dc3545' 
+    });
+}
+
