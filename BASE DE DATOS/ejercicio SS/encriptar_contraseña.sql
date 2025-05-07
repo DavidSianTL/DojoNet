70e1f9f0d077220646b4343044717b572cf844d7
PrisilaFloresDG <pflores@digitalgeko.com>
2025-05-07 12:04:32 -0600

    Ejemplos se session SQL

---
+++ BASE DE DATOS/ejercicio SS/encriptar_contraseña.sql
@@ -0,0 +1,12 @@
+use SistemaSeguridad
+GO
+
+-- Función para encriptar la contraseña (hash SHA-256)
+CREATE FUNCTION encriptar_contraseña (@plain_password VARCHAR(255))
+RETURNS VARCHAR(64)
+AS
+BEGIN
+    -- Retorna el hash de la contraseña en formato hexadecimal (SHA-256)
+    RETURN CONVERT(VARCHAR(64), HASHBYTES('SHA2_256', @plain_password), 2);
+END;
+GO
