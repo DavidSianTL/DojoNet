4af037804662f79fbf83708cb4aaf020154e57de
David Sian LT <dsian@digitalgeko.com>
2025-05-20 12:12:16 -0600

    Cambios menores a proyecto de autenticacion

---
+++ Proyectos de Ejemplo/SistemaAutenticacion/SistemaAutenticacion/Dtos/RolesDtos/RolRegistroRequestDto.cs
@@ -0,0 +1,9 @@
+﻿namespace SistemaAutenticacion.Dtos.RolesDtos
+{
+    public class RolRegistroRequestDto
+    {
+        public string? Nombre { get; set; }
+        public string? Descripcion { get; set; }
+        public DateTime? FechaCreacion { get; set; }
+    }
+}
