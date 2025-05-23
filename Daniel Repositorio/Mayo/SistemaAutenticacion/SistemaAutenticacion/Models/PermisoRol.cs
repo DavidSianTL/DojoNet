94965de2a77d05b617492d1306f8764c9d45560b
Daniel Roblero <droblero@digitalgeko.com>
2025-05-23 13:39:40 -0600

    Creacion Login

---
+++ Daniel Repositorio/Mayo/SistemaAutenticacion/SistemaAutenticacion/Models/PermisoRol.cs
@@ -0,0 +1,12 @@
+﻿namespace SistemaAutenticacion.Models
+{
+    public class PermisoRol
+    {
+        public string? RolId { get; set; } // FK a CustomRolUsuario
+        public int? PermisoId { get; set; } // FK a Permiso
+
+        // Propiedades de navegación
+        public CustomRolUsuario? Rol { get; set; }
+        public Permisos? Permisos { get; set; }
+    }
+}
