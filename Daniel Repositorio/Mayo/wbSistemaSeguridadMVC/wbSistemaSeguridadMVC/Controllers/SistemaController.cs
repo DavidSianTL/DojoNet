7b09e138d69017e9b8ddb192b8dbbe430254c77c
Daniel Roblero <droblero@digitalgeko.com>
2025-05-26 11:01:53 -0600

    cambios

---
+++ Daniel Repositorio/Mayo/wbSistemaSeguridadMVC/wbSistemaSeguridadMVC/Controllers/SistemaController.cs
@@ -0,0 +1,104 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using System.Threading.Tasks;
+using wbSistemaSeguridadMVC.Data;
+using wbSistemaSeguridadMVC.Models;
+
+namespace wbSistemaSeguridadMVC.Controllers
+{
+    public class SistemaController : Controller
+    {
+        private readonly daoSistemasAsync _db;
+
+        public SistemaController() {
+            string connectionString = "Server=LAPTOP-6R9HT6G4;Database=SistemaSeguridad;Integrated Security=True;TrustServerCertificate=True;";
+            _db = new daoSistemasAsync(connectionString);   
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            try
+            {
+                var listaSistemas = await _db.ObtenerSistemasAsync();
+                return View(listaSistemas);
+
+            }
+            catch (Exception ex)
+            {
+                return Content("Error al obtener los Sistemas: " + ex.ToString());
+            }
+
+        }
+
+
+        public async Task<ActionResult> TestConexion()
+        {
+            try
+            {
+                string connectionString = "Server=LAPTOP-6R9HT6G4;Database=SistemaSeguridad;Integrated Security=True;TrustServerCertificate=True;";
+
+                var db = new daoSistemasAsync(connectionString);
+                var sistemas = await db.ObtenerSistemasAsync();
+                return Content($"Conexion exitosa. Se encontraron {sistemas.Count} sistemas");
+            }
+            catch (Exception ex)
+            {
+                return Content("Error al conectar: " + ex.ToString());
+            }
+
+        }
+
+        public ActionResult Crear() {
+
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Crear(Sistema sistema)
+        {
+            if (ModelState.IsValid) {
+                await _db.InsertarSistemaAsync(sistema);
+                return RedirectToAction("Index");
+
+            }
+            return View(sistema);
+
+        }
+
+        public async Task<ActionResult> Editar(int Id)
+        {
+            var sistema = await _db.ObtenerSistemaPorIdAsync(Id);
+            return View(sistema);
+
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Editar(Sistema sistema) 
+        {
+            if (ModelState.IsValid)
+            {
+                await _db.ActualizarSistemaAsync(sistema);
+                return RedirectToAction("Index");
+            }
+            return View(sistema);
+        
+        }
+
+        public async Task<ActionResult> Eliminar(int id)
+        {
+            var sistema = await _db.ObtenerSistemaPorIdAsync(id);
+            return View(sistema);
+
+        }
+
+        [HttpPost, ActionName("Eliminar")]
+        public async Task<ActionResult> ConfirmarEliminar(int id)
+        {
+            await _db.EliminarsistemaAsync(id);
+            return RedirectToAction("Index");
+
+        }
+
+
+    }
+}
