4361c8f16ea1a4d6326c3a84c8d1d35f12fdcc9d
PeGhoste <jopalvarado1@gmail.com>
2025-05-27 11:22:32 -0600

    Iniciando Proyecto Grupal

---
+++ EquipoGeko/ProyectoDojoGeko/ProyectoDojoGeko/Models/UsuarioViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ProyectoDojoGeko.Models
+{
+    [Table("Usuarios")]
+    public class UsuarioViewModel
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int IdUsuario { get; set; }
+
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        [StringLength(100, ErrorMessage = "El campo {0} no puede tener más de {1} caracteres.")]
+        public string Username { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        [StringLength(100, ErrorMessage = "El campo {0} no puede tener más de {1} caracteres.")]
+        public string Password { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public int IdRol { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime FechaCreacion { get; set; } = DateTime.Now;
+
+        public bool Estado { get; set; } = true;
+    }
+}
