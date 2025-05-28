70f6528cee92853be76ffab05cdf005cf432c255
PeGhoste <jopalvarado1@gmail.com>
2025-05-28 11:33:54 -0600

    Se actualizó un modelo

---
+++ EquipoGeko/ProyectoDojoGeko/ProyectoDojoGeko/Models/PermisoViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ProyectoDojoGeko.Models
+{
+    [Table("Permisos")]
+    public class PermisoViewModel
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Column("IdPermiso")]
+        public int IdPermiso { get; set; }
+
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "El campo {0} debe tener entre {2} y {1} caracteres.")]
+        [Column("NombrePermiso")]
+        public string NombrePermiso { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        [StringLength(50, MinimumLength = 10, ErrorMessage = "El campo {0} debe tener entre {5} y {50} caracteres.")]
+        [Column("NombrePermiso")]
+        public string Descripcion { get; set; } = string.Empty;
+
+
+    }
+}
