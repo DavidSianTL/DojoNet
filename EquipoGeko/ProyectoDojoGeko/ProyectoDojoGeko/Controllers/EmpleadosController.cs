8e3fc24f4ec2e642bf7cdd5a801f932a9eca262f
Daniel Roblero <droblero@digitalgeko.com>
2025-05-29 08:59:29 -0600

    Agregacion de controller Empleados, Permisos y Roles

---
+++ EquipoGeko/ProyectoDojoGeko/ProyectoDojoGeko/Controllers/EmpleadosController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProyectoDojoGeko.Controllers
+{
+    public class EmpleadosController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
