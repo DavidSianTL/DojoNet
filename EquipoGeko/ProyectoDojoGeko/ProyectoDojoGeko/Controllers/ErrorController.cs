93b5248b0ea1db2a636f96b1d64a65eb8de6bc1e
Erickbs21 <ebarrera@digitalgeko.com>
2025-05-29 16:02:43 -0600

    Agregando el error 404 y solucinando errores de vistas

--- EquipoGeko/ProyectoDojoGeko/ProyectoDojoGeko/Controllers/ErrorController.cs
+++ EquipoGeko/ProyectoDojoGeko/ProyectoDojoGeko/Controllers/ErrorController.cs
@@ -11,6 +11,13 @@ namespace ProyectoDojoGeko.Controllers
             return View("Error404");
         }
 
+        [Route("Error/403")] // Nueva ruta para el error 403
+        public IActionResult Error403()
+        {
+            Response.StatusCode = 403;
+            return View("Error403"); // Llama a la nueva vista Error403.cshtml
+        }
+
         [Route("Error/{code:int}")]
         public IActionResult Error(int code)
         {
@@ -20,7 +27,13 @@ namespace ProyectoDojoGeko.Controllers
             {
                 case 404:
                     return View("Error404");
+                case 403:
+                    return View("Error403"); // Añadido caso para 403
+                // Puedes añadir mas casos para otros codigos de error aqui
+                // case 500:
+                //     return View("Error500");
                 default:
+                    // Por defecto, si no es un error especifico, muestra la pagina 404 o una generica
                     return View("Error404");
             }
         }
