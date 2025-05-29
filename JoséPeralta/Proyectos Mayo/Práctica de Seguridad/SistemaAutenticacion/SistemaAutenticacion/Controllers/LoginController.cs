0ecec2f4a4af497d77fcf9c91d640d4a99fa58f1
PeGhoste <jopalvarado1@gmail.com>
2025-05-29 10:43:51 -0600

    Guardando cambios antes de hacer un pull

--- JoséPeralta/Proyectos Mayo/Práctica de Seguridad/SistemaAutenticacion/SistemaAutenticacion/Controllers/LoginController.cs
+++ JoséPeralta/Proyectos Mayo/Práctica de Seguridad/SistemaAutenticacion/SistemaAutenticacion/Controllers/LoginController.cs
@@ -3,17 +3,37 @@ using Microsoft.AspNetCore.Authentication;
 using System.Security.Claims;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using SistemaAutenticacion.Data.Usuario;
+using SistemaAutenticacion.Dtos.UsuarioDto;
+using Microsoft.AspNetCore.Http.HttpResults;
 
 namespace SistemaAutenticacion.Controllers
 {
     public class LoginController : Controller
     {
-        // GET: Index
+        // Repositorio para manejar las operaciones de usuario
+        private readonly IUsuariosRepository _usuariosRepository;
+
+        // Constructor que recibe el repositorio de usuarios
+        public LoginController(IUsuariosRepository usuariosRepository)
+        {
+            _usuariosRepository = usuariosRepository;
+        }
+
+        // GET: Login/Index
         [HttpGet]
         public IActionResult Index()
         {
             return View();
         }
+
+        // GET: Login/Usuarios
+        [HttpGet]
+        public async Task<ActionResult<UsuarioResponseDto>> Usuarios()
+        {
+            return await _usuariosRepository.GetUsuario();
+        }
+
 
 
     }
--- JoséPeralta/Proyectos Mayo/Práctica de Seguridad/SistemaAutenticacion/SistemaAutenticacion/Data/Usuario/UsuariosRepository.cs
+++ JoséPeralta/Proyectos Mayo/Práctica de Seguridad/SistemaAutenticacion/SistemaAutenticacion/Data/Usuario/UsuariosRepository.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using SistemaAutenticacion.Dtos.UsuarioDto;
 using SistemaAutenticacion.Models;
 using SistemaAutenticacion.Tokens;
@@ -8,6 +9,15 @@ namespace SistemaAutenticacion.Data.Usuario
 
     public interface IUsuariosRepository
     {
+
+        // Método para obtener el usuario en sesión
+        Task<UsuarioResponseDto> GetUsuario();
+        // Método para iniciar sesión
+        Task<UsuarioResponseDto> Login(UsuarioLoginRequestDto usuarioLogin);
+        // Método para registrar un nuevo usuario
+        Task<UsuarioResponseDto> Registrar(UsuarioRegistroRequestDto usuarioRegistro);
+        // Método para obtener el rol del usuario en sesión
+        Task<string> ObtenerRolUsuarioEnSesion();
 
     }
 
@@ -71,6 +81,99 @@ namespace SistemaAutenticacion.Data.Usuario
             return TransformerUserToUserDto(user!); // Se usa el operador de null-forgiving (!) para indicar que no es nulo
         }
 
+        // Método de inicio de sesión
+        public async Task<UsuarioResponseDto> Login(UsuarioLoginRequestDto usuarioLogin)
+        {
+
+            // Creamos un método para obtener el usuario en sesión
+            var usuario = await _userManager.FindByEmailAsync(usuarioLogin.Email!);
+
+            // Validamos que el usuario exista
+            if (usuario == null)
+            {
+                throw new Exception("No se encontró una cuenta asociada a este correo: " + usuario.Email);
+            }
+
+            // Validamos que la contraseña sea correcta
+            var resultado = await _signInManager.CheckPasswordSignInAsync(usuario, usuarioLogin.Password!, false);
+
+            // Si el resultado es exitoso, se genera el token y se retorna el usuario transformado a DTO
+            if (resultado.Succeeded)
+            {
+                return TransformerUserToUserDto(usuario!);
+            }
+
+            // Si el resultado no es exitoso, se lanza una excepción
+            throw new Exception("Credenciales incorrectas. Por favor, verifique que su correo sea correcto al igual que su contraseña...");
+
+        }
+
+        // Método para registrar un nuevo usuario
+        public async  Task<UsuarioResponseDto> Registrar(UsuarioRegistroRequestDto usuarioRegistro)
+        {
+
+            // Validamos que el correo no exista usando el "_appDbContext" para consultar la base de datos
+            var emailExistente = await _appDbContext.Users.Where(x => x.Email == usuarioRegistro.Email).AnyAsync();
+
+            // Si el usuario ya existe, se lanza una excepción
+            if (emailExistente)
+            {
+                throw new Exception("Ya existe un usuario registrado con este correo: " + usuarioRegistro.Email);
+            }
+
+            // Creamos un nuevo usuario con los datos del registro
+            var usuario = new UsuarioViewModel
+            {
+                Nombres = usuarioRegistro.Nombres,
+                Apellidos = usuarioRegistro.Apellidos,
+                Email = usuarioRegistro.Email,
+                UserName = usuarioRegistro.Email, // Usamos el email como nombre de usuario
+                Telefono = usuarioRegistro.Telefono,
+                FechaCreacion = DateTime.UtcNow // Usamos el UtcNow para referirnos a la fecha y hora actual en formato UTC(hora universal coordinada)
+            };
+
+            // Agregamos el usuario a la base de datos
+            var resultado = await _userManager.CreateAsync(usuario, usuarioRegistro.Password!);
+
+            // Si el resultado es exitoso, se genera el token y se retorna el usuario transformado a DTO
+            if (resultado.Succeeded)
+            {
+                return TransformerUserToUserDto(usuario);
+            }
+
+            // Si el resultado no es exitoso, se lanza una excepción con los errores
+            throw new Exception("Error al registrar el usuario. Por favor, verifica los datos ingresados e intenta de nuevo.");
+
+        }
+
+        // Método para obtener el rol del usuario que está en sesión (Vendedor, Administrador, etc.)
+        // Retornamos el primer rol que el usuario tenga asignado
+        public async Task<string> ObtenerRolUsuarioEnSesion()
+        {
+
+            // Obtener el usuario en sesión
+            var usuario = await _userManager.FindByIdAsync(_usuarioSesion.ObtenerUsuarioSesion());
+
+            // Validamos que el usuario exista
+            if (usuario is null)
+            {
+                throw new Exception("El token que se ha proporcionado, no corresponde a ningún usuario en sesión");
+            }
+
+            // Obtenemos los roles del usuario
+            var roles = await _userManager.GetRolesAsync(usuario);
+
+            // Validamos que el usuario tenga al menos un rol
+            if (roles.Count == 0)
+            {
+                throw new Exception("El usuario no tiene roles asignados.");
+            }
+
+            // Retornamos el primer rol del usuario
+            return roles.FirstOrDefault(); // Si no hay roles
+
+
+        }
 
 
     }
