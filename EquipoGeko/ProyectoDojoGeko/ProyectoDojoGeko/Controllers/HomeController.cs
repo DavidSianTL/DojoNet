f6c46dae86c08437ca06b60261124ee6a0db64ff
Anika Escoto <aescoto@digitalgeko.com>
2025-05-30 10:02:18 -0600

    Barra lateral

--- EquipoGeko/ProyectoDojoGeko/ProyectoDojoGeko/Controllers/HomeController.cs
+++ EquipoGeko/ProyectoDojoGeko/ProyectoDojoGeko/Controllers/HomeController.cs
@@ -30,5 +30,9 @@ namespace ProyectoDojoGeko.Controllers
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
 
+        public IActionResult Menu()
+        {
+            return View();
+        }
     }
 }
