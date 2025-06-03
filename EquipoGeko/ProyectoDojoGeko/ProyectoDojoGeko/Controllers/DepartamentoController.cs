b29e1a1d975f72c38c0f406e2a5d7adaa22dc2e5
Carlos <ccaal@digitalgeko.com>
2025-06-03 15:03:03 -0600

    Subiendo controller de departamento y sistema

--- EquipoGeko/ProyectoDojoGeko/ProyectoDojoGeko/Controllers/DepartamentoController.cs
+++ EquipoGeko/ProyectoDojoGeko/ProyectoDojoGeko/Controllers/DepartamentoController.cs
@@ -1,12 +1,47 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ProyectoDojoGeko.Data;
+using ProyectoDojoGeko.Filters;
 
 namespace ProyectoDojoGeko.Controllers
 {
+    [AuthorizeSession]
     public class DepartamentoController : Controller
     {
+        private readonly daoDepartamentoWSAsync _daoDepartamento;
+
+        public DepartamentoController()
+        {
+            string _connectionString = "Server=localhost;Database=DBProyectoGrupalDojoGeko;Trusted_Connection=True;TrustServerCertificate=True;";
+            _daoDepartamento = new daoDepartamentoWSAsync(_connectionString);
+        }
+
+        [HttpGet]
         public IActionResult Index()
         {
             return View();
         }
+
+        [AuthorizeRole("SuperAdmin", "Admin")]
+        [HttpGet]
+        public IActionResult AgregarDepartamento()
+        {
+            return View("Agregar", "Departamento");
+        }
+
+        [AuthorizeRole("SuperAdmin")]
+        [HttpGet]
+        public IActionResult EditarDepartamento()
+        {
+            return View("Editar", "Departamento");
+        }
+
+        [HttpPost]
+        public IActionResult EliminarDepartamento(int Id)
+        {
+            // Lógica para eliminar departamento
+            return RedirectToAction("Index");
+        }
     }
 }
+
+
--- EquipoGeko/ProyectoDojoGeko/ProyectoDojoGeko/Controllers/SistemaController.cs
+++ EquipoGeko/ProyectoDojoGeko/ProyectoDojoGeko/Controllers/SistemaController.cs
@@ -1,12 +1,45 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ProyectoDojoGeko.Data;
+using ProyectoDojoGeko.Filters;
 
 namespace ProyectoDojoGeko.Controllers
 {
+    [AuthorizeSession]
     public class SistemaController : Controller
     {
+        private readonly daoSistemaWSAsync _daoSistema;
+
+        public SistemaController()
+        {
+            string _connectionString = "Server=localhost;Database=DBProyectoGrupalDojoGeko;Trusted_Connection=True;TrustServerCertificate=True;";
+            _daoSistema = new daoSistemaWSAsync(_connectionString);
+        }
+
+        [HttpGet]
         public IActionResult Index()
         {
             return View();
+        }
+
+        [AuthorizeRole("SuperAdmin", "Admin")]
+        [HttpGet]
+        public IActionResult AgregarSistema()
+        {
+            return View("Agregar", "Sistema");
+        }
+
+        [AuthorizeRole("SuperAdmin")]
+        [HttpGet]
+        public IActionResult EditarSistema()
+        {
+            return View("Editar", "Sistema");
+        }
+
+        [HttpPost]
+        public IActionResult EliminarSistema(int Id)
+        {
+            // Lógica para eliminar sistema
+            return RedirectToAction("Index");
         }
     }
 }
