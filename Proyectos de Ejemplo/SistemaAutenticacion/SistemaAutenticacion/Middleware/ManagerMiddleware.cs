9544cdeae2ba6174eb3ab964bcb473f38b18b2f8
David Sian <davidsian804@gmail.com>
2025-06-04 17:02:59 -0600

    Implementacion de logica para el manejo de los middlewares

---
+++ Proyectos de Ejemplo/SistemaAutenticacion/SistemaAutenticacion/Middleware/ManagerMiddleware.cs
@@ -0,0 +1,71 @@
+﻿using Newtonsoft.Json;
+using System.Net;
+using System.Text.Json;
+
+namespace SistemaAutenticacion.Middleware
+{
+    /// <summary>
+    /// Escuchador de excepciones
+    /// </summary>
+    public class ManagerMiddleware
+    {
+        private readonly RequestDelegate _requestDelegateNext;
+        private readonly ILogger<ManagerMiddleware> _logger;
+
+        public ManagerMiddleware(RequestDelegate requestDelegateNext, ILogger<ManagerMiddleware> logger)
+        {
+            _requestDelegateNext = requestDelegateNext;
+            _logger = logger;
+        }
+
+        private async Task ManagerExceptionAsync(HttpContext httpContext, Exception exception, ILogger<ManagerMiddleware> logger)
+        {
+            //Objeto que define el tipo de error generico
+            object? errores = null;
+
+            switch (exception)
+            {
+                //Errores de tipo MiddlewareException
+                case MiddleException middlewareException:
+                    logger.LogError(exception, "Middleware Error");
+                    errores = middlewareException.Errores;
+                    httpContext.Response.StatusCode = (int)middlewareException.Codigo;
+                break;
+
+                //Errores genericos
+                case Exception ex:
+                    logger.LogError(exception, "Error de servidor");
+                    errores = string.IsNullOrEmpty(ex.Message) ? "Error": ex.Message;
+                    httpContext.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                break;
+
+            }
+
+            //Escribir el error en el cuerpo de la respuesta
+            httpContext.Response.ContentType = "application/json";
+
+            var resultado = string.Empty;
+
+            if (errores != null)
+            {
+                resultado = JsonConvert.SerializeObject(new { errores });
+            }
+
+            //Enviar la respuesta al cliente
+            await httpContext.Response.WriteAsync(resultado);
+        }
+
+        public async Task Invoke(HttpContext httpContext)
+        {
+            try
+            {
+                await _requestDelegateNext(httpContext);
+            }
+            catch (Exception ex)
+            {
+                await ManagerExceptionAsync(httpContext, ex, _logger);
+            }
+        }
+
+    }
+}
---
+++ Proyectos de Ejemplo/SistemaAutenticacion/SistemaAutenticacion/Middleware/MiddleException.cs
@@ -0,0 +1,16 @@
+﻿using System.Net;
+
+namespace SistemaAutenticacion.Middleware
+{
+    public class MiddleException: Exception
+    {
+        public HttpStatusCode Codigo { get; set; }
+        public object? Errores { get; set; }
+
+        public MiddleException(HttpStatusCode codigo, object? errores)
+        {
+            Codigo = codigo;
+            Errores = errores;
+        }
+    }
+}
