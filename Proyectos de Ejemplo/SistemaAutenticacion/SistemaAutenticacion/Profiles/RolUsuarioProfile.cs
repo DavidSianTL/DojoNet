1a76ba136f2c1f79d2f6ab26305b117e64796bd2
David Sian <davidsian804@gmail.com>
2025-06-05 17:15:15 -0600

    Cambios antes del pull

---
+++ Proyectos de Ejemplo/SistemaAutenticacion/SistemaAutenticacion/Profiles/RolUsuarioProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using SistemaAutenticacion.Dtos.RolesDtos;
+using SistemaAutenticacion.Models;
+
+namespace SistemaAutenticacion.Profiles
+{
+    public class RolUsuarioProfile: Profile
+    {
+        public RolUsuarioProfile()
+        {
+            //Modelo Origen y modelo destino
+            CreateMap<CustomRolUsuario, RolResponseDto>();
+            CreateMap<RolResponseDto, CustomRolUsuario>();
+        }
+    }
+}
