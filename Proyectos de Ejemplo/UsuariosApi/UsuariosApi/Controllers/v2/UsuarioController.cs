3953261a235aff94d243da1e9fd1dab312a7f462
PrisilaFloresDG <pflores@digitalgeko.com>
2025-06-11 17:45:32 -0600

    Ejemplos de ApiRest Versiones

---
+++ Proyectos de Ejemplo/UsuariosApi/UsuariosApi/Controllers/v2/UsuarioController.cs
@@ -0,0 +1,130 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Identity.Client;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System;
+using UsuariosApi.DAO;
+using UsuariosApi.Models;
+using Asp.Versioning;
+
+namespace UsuariosApi.Controllers.v2
+{
+    [ApiVersion("2.0")]
+    [Route("api/v{version:ApiVersion}/[controller]")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+        private readonly daoUsuariosAsync _DaoUsuarios;
+
+        public UsuarioController(daoUsuariosAsync daoUsuarioP)
+        {
+            _DaoUsuarios = daoUsuarioP;
+        }
+        [HttpGet("version2")]
+        public IActionResult GetVersion()
+        {
+            var apiVersion = HttpContext.GetRequestedApiVersion()?.ToString() ?? "No version";
+            return Ok(new { version = apiVersion });
+            // return Ok (new { version = apiVersion.ToString() });
+
+        }
+
+        [HttpGet("v2")]
+        [MapToApiVersion("2.0")]
+        public async Task<ActionResult<List<Usuario>>> Get()
+        {
+            try
+            {
+                var usuarios = await _DaoUsuarios.ObtenerUsuariosAsync();
+                return Ok(usuarios);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new { message = "ERROR: Al obtener usuarios. ", detalle = ex.Message });
+            }
+
+        }
+
+        [HttpPost("v2")]
+        [MapToApiVersion("2.0")]
+        public async Task<IActionResult> Post([FromBody] Usuario usuario)
+        {
+
+            try
+            {
+                if (!ModelState.IsValid) 
+                {
+                    return BadRequest(new { code = 400, mensaje = "Datos de usuario son invalidos" });
+                }
+                if (string.IsNullOrWhiteSpace(usuario.UsuarioLg))
+                {
+                    return BadRequest(new { code = 400, mensaje = "El login del usuario no  puede ser vacio o nulo" });
+                }
+                
+                
+                if (ModelState.IsValid)
+                {
+                    await _DaoUsuarios.InsertarUsuarioAsync(usuario);
+                    return Ok(new { mensaje = "Usuario creado exitosamente." });
+
+                }
+                else
+                {
+                    return BadRequest(ModelState);
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new { mensaje = "ERROR: Error al crear usuario.", detalle = ex.Message });
+
+            }
+
+        }
+
+        [HttpPut("v2/{id}")]
+        [MapToApiVersion("2.0")]
+        public async Task<IActionResult> Put(int id, [FromBody] Usuario usuario)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    usuario.IdUsuario = id;
+                    await _DaoUsuarios.ActualizarUsuarioAsync(usuario);
+                    return Ok(new { mensaje = "Usuario actualizado correctamente." });
+
+                }
+                else
+                {
+                    return BadRequest(ModelState);
+                }
+            }
+            catch
+            {
+                return StatusCode(500, new { mensaje = "ERROR: Al actualizar usuario" });
+            }
+
+        }
+
+        [HttpDelete("v2/{id}")]
+        [MapToApiVersion("2.0")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            try
+            {
+                await _DaoUsuarios.EliminarUsuarioAsync(id);
+                return Ok(new { mensaje = "Usuario eliminado exitosamente." });
+            }
+            catch
+            {
+                return StatusCode(500, new { mensaje = "ERROR: Al eliminar usuario" });
+            }
+
+        }
+
+
+       
+    }
+}
---
+++ Proyectos de Ejemplo/UsuariosApi/UsuariosApi/DAO/daoUsuarioAsync.cs
@@ -0,0 +1,248 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Collections.Generic;
+using System;
+using System.Threading.Tasks;
+using UsuariosApi.Models;
+using UsuariosApi.Data;
+using UsuariosApi.Exceptions;
+
+
+
+namespace UsuariosApi.DAO
+{
+    public class daoUsuariosAsync
+    {
+        private readonly DbConnection _db;
+        public const int ESTADO_ELIMINADO = 4;
+
+        public daoUsuariosAsync(DbConnection db)
+        {
+            _db = db;
+
+        }
+        public async Task<List<Usuario>> ObtenerUsuariosAsync()
+        {
+            var ListaU = new List<Usuario>();
+            string query = "";
+
+            query = $@"SELECT u.id_usuario as id_usuario,
+                        u.usuario  as usuario, 
+                        u.nom_usuario as nom_usuario, 
+                        u.fk_id_estado as fk_id_estado, 
+                        u.fecha_creacion as fecha_creacion, 
+                        e.descripcion as descripcion
+                    FROM Usuarios u 
+                    INNER JOIN Estado_Usuario e 
+                        ON u.fk_id_estado = e.id_estado";
+
+            using var cnn = _db.GetConnection();
+            try
+            {
+               await cnn.OpenAsync();
+                var cmd = new SqlCommand(query, cnn);
+                using var reader = await cmd.ExecuteReaderAsync();
+                while (await reader.ReadAsync())
+                {
+                    ListaU.Add(new Usuario
+                    {
+                        IdUsuario = (int)reader["id_usuario"],
+                        UsuarioLg = reader["usuario"].ToString(),
+                        Nom_Completo = reader["nom_usuario"].ToString(),
+                        Fk_id_estado = (int)reader["fk_id_estado"],
+                        Fecha_creacion = (DateTime)reader["fecha_creacion"],
+                        Descripcion = reader["descripcion"].ToString()
+
+                    });
+                }
+
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("ERROR: AL OBTENER USUARIOS." + ex.Message, ex);
+
+            }
+            return ListaU;
+
+        }
+
+        public async Task InsertarUsuarioAsync(Usuario usuario)
+        {
+            string query = "";
+            var contraseniaHash = BCrypt.Net.BCrypt.HashPassword(usuario.Contrasenia);
+
+
+
+            query = $@"INSERT INTO Usuarios (
+                        usuario
+                        , nom_usuario
+                        , contrasenia
+                        , fk_id_estado
+                        , fecha_creacion) 
+                    VALUES (@Usuario
+                        , @NombreU
+                        , @Contrasenia
+                        , @Estado
+                        , GETDATE())";
+
+            try
+            {
+                using var conn = _db.GetConnection();
+                await conn.OpenAsync();
+                var cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@Usuario", usuario.UsuarioLg);
+                cmd.Parameters.AddWithValue("@NombreU", usuario.Nom_Completo);
+                cmd.Parameters.AddWithValue("@Contrasenia", contraseniaHash);
+                cmd.Parameters.AddWithValue("@Estado", usuario.Fk_id_estado);
+
+                await cmd.ExecuteNonQueryAsync();
+
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("ERROR: AL INSERTAR USUARIOS." + ex.Message, ex);
+
+            }
+
+        }
+        private async Task<bool> ValidarContraseniaCambioAsync(string ContraseniaCambio, int IdUsuario)
+        {
+            bool contraseniaCambio = false;
+            string contraseniaHashAlmacenada = "";
+            int filasConsultadas = 0;//v3
+            string query = $@"SELECT  contrasenia
+                                FROM Usuarios 
+                               WHERE id_usuario = @Id_Usuario";
+
+            try
+            {
+                using (var conn = _db.GetConnection())
+                {
+                    await conn.OpenAsync();
+                    var cmd = new SqlCommand(query, conn);
+                    cmd.Parameters.AddWithValue("@Id_Usuario", IdUsuario);
+                    using var reader = await cmd.ExecuteReaderAsync();
+                    if (await reader.ReadAsync())
+                    {
+                        
+                        contraseniaHashAlmacenada = reader["contrasenia"].ToString();
+                    }
+                }
+
+                if (contraseniaHashAlmacenada == "")
+                {
+                    throw new NotFoundException($"Usuario con id {IdUsuario} no encontrado.");
+                }
+                
+                contraseniaCambio = !BCrypt.Net.BCrypt.Verify(ContraseniaCambio, contraseniaHashAlmacenada);
+
+               
+                
+
+
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("ERROR: AL VALIDAR CAMBIO DE CONTRASEÑA." + ex.Message, ex);
+
+            }
+
+
+            return contraseniaCambio;
+        }
+
+
+        public async  Task ActualizarUsuarioAsync(Usuario usuario)
+        {
+
+            bool CambioContrasenia = false;
+            string query = "";
+            int filasAfectadas = 0;//v3
+
+
+
+
+            query = $@"UPDATE Usuarios SET 
+                        usuario = @UsuarioL
+                        , nom_usuario = @Nombre
+                        , fk_id_estado = @Estado";
+            if (!string.IsNullOrWhiteSpace(usuario.Contrasenia))
+            {
+                CambioContrasenia = await ValidarContraseniaCambioAsync(usuario.Contrasenia, usuario.IdUsuario);
+            }
+
+            if (CambioContrasenia)
+            {
+                query += ", contrasenia = @ContraseniaA";
+
+            }
+            query += " WHERE id_usuario = @Id_Usuario";
+
+            try
+            {
+                using var conn = _db.GetConnection();
+                await conn.OpenAsync();
+
+                var cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@Id_Usuario", usuario.IdUsuario);
+                cmd.Parameters.AddWithValue("@UsuarioL", usuario.UsuarioLg);
+                cmd.Parameters.AddWithValue("@Nombre", usuario.Nom_Completo);
+                cmd.Parameters.AddWithValue("@Estado", usuario.Fk_id_estado);
+
+                if (CambioContrasenia)
+                {
+                    var nuevaHash = BCrypt.Net.BCrypt.HashPassword(usuario.Contrasenia);
+                    cmd.Parameters.AddWithValue("@ContraseniaA", nuevaHash);
+                }
+                //v3
+                filasAfectadas = await cmd.ExecuteNonQueryAsync();
+                if (filasAfectadas == 0)
+                {
+                    throw new NotFoundException($"Usuario con id {usuario.IdUsuario} no encontrado.");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("ERROR: AL ACTUALIZAR USUARIOS." + ex.Message, ex);
+
+            }
+        }
+
+
+        public async Task EliminarUsuarioAsync(int id)
+        {
+
+            string query = "";
+            int filasAfectadas = 0;
+
+            query = $@"UPDATE Usuarios SET 
+                            fk_id_estado = ";
+            query += ESTADO_ELIMINADO;
+            query += " WHERE id_usuario = @Id_Usuario";
+
+            try
+            {
+                using var conn = _db.GetConnection();
+                await conn.OpenAsync();
+                var cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@Id_Usuario", id);
+               
+               //v3 
+                filasAfectadas = await cmd.ExecuteNonQueryAsync();
+                if (filasAfectadas == 0)
+                {
+                    throw new NotFoundException($"Usuario con id {id} no encontrado.");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("ERROR: AL ELIMINAR USUARIOS." + ex.Message, ex);
+
+            }
+
+        }
+
+    }
+}
+
