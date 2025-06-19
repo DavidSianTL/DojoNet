40d40c80b7b2f54639b28cec18b8d175e4e3292f
Estiben-Hernandez <jhernandez@digitalgeko.com>
2025-06-19 10:56:42 -0600

    feat(Token): Agregar JWTGenerator.cs para generar JWT's

--- Prueba1-Junior/Entity_Framework/AuthenticationSystem/AuthenticationSystem/Token/JWTGenerator.cs
+++ Prueba1-Junior/Entity_Framework/AuthenticationSystem/AuthenticationSystem/Token/JWTGenerator.cs
@@ -1,14 +1,49 @@
-﻿namespace AuthenticationSystem.Token
+﻿using AuthenticationSystem.Models;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace AuthenticationSystem.Token
 {
 
-    public interface IJWTGeneratoru
-    {
-        string GenerateToken();
-    }
+	public interface IJWTGenerator
+	{
+		string GenerateToken(Usuario user);
+	}
 
 
-    public class JWTGenerator
-    {
+	public class JWTGenerator : IJWTGenerator
+	{
+		public string GenerateToken(Usuario user)
+		{
+			// Crear claims para el Json Web Token con los datos del usuario
+			var claims = new List<Claim>()
+			{
+				new Claim(JwtRegisteredClaimNames.NameId, user.UserName!),
+				new Claim("userId", user.Id),
+				new Claim("email", user.Email!)
+			};
 
-    }
+			// Se crea la palabra clave y se codifica
+			var SecurityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("$EstaEsmiClaveSeguraDe32Bytesxd!"));
+
+			// Se encripta la palabra clave ya codificada
+			var EncryptedSecurityKey = new SigningCredentials(SecurityKey, SecurityAlgorithms.HmacSha256Signature);
+
+			// Se crea la configuración del token y se firma
+			var TokenDescriptor = new SecurityTokenDescriptor()
+			{
+				Subject = new ClaimsIdentity(claims), // Header (datos del usuario)
+				Expires = DateTime.UtcNow.AddDays(1), // Exoira en 1 día 
+				SigningCredentials = EncryptedSecurityKey // Firma del token
+			};
+			// Finalmente se crea el token con la configuración
+			var TokenHandler = new JwtSecurityTokenHandler();
+			var token = TokenHandler.CreateToken(TokenDescriptor);
+
+			// Se retorna el token como string
+			return TokenHandler.WriteToken(token);
+		}
+	}
 }
