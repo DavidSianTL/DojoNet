368bd70762f112cf3aaa0d8ae6fa52e85327eb5b
Daniel Roblero <droblero@digitalgeko.com>
2025-06-20 02:10:56 -0600

    Cambio de modelos

--- Daniel Repositorio/Mes Junio/ApiClinicaMedica/ApiClinicaMedica/Models/Medico.cs
+++ Daniel Repositorio/Mes Junio/ApiClinicaMedica/ApiClinicaMedica/Models/Medico.cs
@@ -6,12 +6,15 @@ namespace ApiClinicaMedica.Models
     {
         [Key]
         public int IdMedico { get; set; }
+
         public string Nombre { get; set; }
+
         public string Email { get; set; }
 
         public int EspecialidadId { get; set; }
-        public Especialidad Especialidad { get; set; }
 
-        public ICollection<Cita> Citas { get; set; }
+        public Especialidad? Especialidad { get; set; }
+
+        public ICollection<Cita>? Citas { get; set; }
     }
 }
