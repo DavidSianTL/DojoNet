ba52af431a21cfec76bc6c5ccc0971568ec51249
Estiben-Hernandez <jhernandez@digitalgeko.com>
2025-06-20 10:03:21 -0600

    feat(Services): Agregar DbConnectionService.cs para manejar la conexión y consultas a la DB

--- Prueba1-Junior/APIS/Desarrollo/API REST/ClinicaMedicaAPIREST/ClinicaMedicaAPIREST/Program.cs
+++ Prueba1-Junior/APIS/Desarrollo/API REST/ClinicaMedicaAPIREST/ClinicaMedicaAPIREST/Program.cs
@@ -1,7 +1,12 @@
+using ClinicaMedicaAPIREST.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+// Add the DbConnectionService as a IDbConnectionService implementation
+builder.Services.AddScoped<IDbConnectionService, DbConnectionService>();
 
 var app = builder.Build();
 
---
+++ Prueba1-Junior/APIS/Desarrollo/API REST/ClinicaMedicaAPIREST/ClinicaMedicaAPIREST/Services/DbConnectionService.cs
@@ -0,0 +1,115 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Data;
+using System.Reflection.Metadata;
+
+namespace ClinicaMedicaAPIREST.Services
+{
+
+	public interface IDbConnectionService
+	{
+		Task<DataSet> ExecuteStoredProcedureAsync(string sp);
+		Task<DataSet> ExecuteStoredProcedureAsync(string sp, List<SqlParameter> parameters);
+		Task<bool> ExecuteStoredProcedureNonQueryAsync(string sp, List<SqlParameter>? parameters = null);
+	}
+
+	public class DbConnectionService : IDbConnectionService
+	{
+		private readonly ILogger<DbConnectionService> _logger;
+		private readonly string _connectionString;
+		public DbConnectionService(IConfiguration configuration, ILogger<DbConnectionService> logger)
+		{
+			_logger = logger;
+			_connectionString = configuration.GetConnectionString("DefaultConnection")!; // Se obtiene la cadena de conexión desde el appsettings.
+		}
+
+
+
+
+
+
+	  // Metodo para ejecutar un stored procedure que retorna datos pero no necesita parametros      
+		public async Task<DataSet> ExecuteStoredProcedureAsync(string sp)
+		{
+			var ds = new DataSet();
+
+			try
+			{
+
+				using SqlConnection cnn = new SqlConnection(_connectionString);
+				
+				await cnn.OpenAsync();
+				SqlDataAdapter adapter = new SqlDataAdapter(sp, cnn);
+
+				adapter.SelectCommand.CommandType = CommandType.StoredProcedure;
+                adapter.Fill(ds);
+				
+				
+			}catch (Exception ex)
+			{
+				_logger.LogError(ex, "Error al ejecutar el Stored Procedure {sp}", sp);
+			}
+
+
+			return ds;
+		}
+
+
+		// Metodo para ejecutar un stored procedure que retorna datos y necesita parametros
+		public async Task<DataSet> ExecuteStoredProcedureAsync(string sp, List<SqlParameter> parameters)
+		{
+			var ds = new DataSet();
+			try
+			{
+				using SqlConnection cnn = new SqlConnection(_connectionString);
+				
+				await cnn.OpenAsync();
+
+				// Se crea un SqlDataAdapter para ejecutar el stored procedure
+				SqlDataAdapter adapter = new SqlDataAdapter(sp, cnn); 
+				adapter.SelectCommand.CommandType = CommandType.StoredProcedure;
+				if (parameters != null && parameters.Count > 0)
+				{
+					adapter.SelectCommand.Parameters.AddRange(parameters.ToArray());
+				}
+				adapter.Fill(ds);
+				
+				
+			}catch (Exception ex)
+			{
+				_logger.LogError(ex, "Error al ejecutar el SP {sp}", sp);
+			}
+
+			return ds;
+		}
+
+		// Metodo para ejecutar un stored procedure que no retorna datos y necesita parametros
+		public async Task<bool> ExecuteStoredProcedureNonQueryAsync(string sp, List<SqlParameter>? parameters = null)
+		{
+			try
+			{
+				using SqlConnection cnn = new SqlConnection(_connectionString);
+				
+				await cnn.OpenAsync();
+				using (SqlCommand cmd = new SqlCommand(sp, cnn))
+				{
+					cmd.CommandType = CommandType.StoredProcedure;
+					if (parameters != null && parameters.Count > 0)
+					{
+						cmd.Parameters.AddRange(parameters.ToArray());
+					}
+					await cmd.ExecuteNonQueryAsync();
+				}
+				
+				return true;
+			}
+			catch (Exception ex)
+			{
+				_logger.LogError(ex, "Error al ejecutar el Stored procedure {sp}", sp);
+				return false;
+			}
+		}
+
+
+
+	}
+}
