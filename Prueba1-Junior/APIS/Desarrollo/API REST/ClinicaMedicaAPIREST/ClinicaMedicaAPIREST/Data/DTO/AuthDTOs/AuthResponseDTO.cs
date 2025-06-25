edf7ff9048037cdea0dfb601996975e9ac214ab3
Estiben-Hernandez <jhernandez@digitalgeko.com>
2025-06-25 08:24:57 -0600

    feat(AuthDTOs): Agregar DTOs LoginRequestDTO.cs y AuthResponseDTO.cs para transportar datos de autenticacion

---
+++ Prueba1-Junior/APIS/Desarrollo/API REST/ClinicaMedicaAPIREST/ClinicaMedicaAPIREST/Data/DTO/AuthDTOs/AuthResponseDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace ClinicaMedicaAPIREST.Data.DTO.AuthDTOs
+{
+    public class AuthResponseDTO
+    {
+        public string Username { get; set; } = null!;
+        public string Role {  get; set; } = null!;
+        public string Token { get; set; } = null!;
+        public DateTime ExpirationToken { get; set; }
+    }
+}
---
+++ Prueba1-Junior/APIS/Desarrollo/API REST/ClinicaMedicaAPIREST/ClinicaMedicaAPIREST/Data/DTO/AuthDTOs/LoginRequestDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace ClinicaMedicaAPIREST.Data.DTO.AuthDTOs
+{
+    public class LoginRequestDTO
+    {
+        public string Username { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+
+    }
+}
