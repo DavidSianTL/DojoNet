46157f0272741e89ef0e6a16887226d824a711c4
Estiben-Hernandez <jhernandez@digitalgeko.com>
2025-06-25 10:56:32 -0600

    feat(Controllers): Implementar AuthService en AuthController

---
+++ Prueba1-Junior/APIS/Desarrollo/API REST/ClinicaMedicaAPIREST/ClinicaMedicaAPIREST/Controllers/AuthController.cs
@@ -0,0 +1,31 @@
+﻿using ClinicaMedicaAPIREST.Data.DTO.AuthDTOs;
+using ClinicaMedicaAPIREST.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ClinicaMedicaAPIREST.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthService _authService;
+
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+
+
+        [HttpPost]
+        public async Task<IActionResult> Login([FromBody] LoginRequestDTO loginRequest)
+        {
+            var user = await _authService.LoginAsync(loginRequest);
+
+            if (user == null) return Unauthorized("Credenciales inválidas");
+
+            return Ok(user);
+        }
+    }
+}
