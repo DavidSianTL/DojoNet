ba1ae3c2b0818426cbcf523e1042d7c8660c7833
Estiben-Hernandez <jhernandez@digitalgeko.com>
2025-06-27 09:02:10 -0600

    feat(DB): Agregar script para crear usuario para la API en la base de datos

---
+++ Prueba1-Junior/APIS/Desarrollo/API REST/ClinicaMedica_db/CreateLoginForAPI.sql
@@ -0,0 +1,22 @@
+USE master;
+GO
+
+CREATE LOGIN clinicaUser WITH PASSWORD = 'drugsxd';
+GO 
+
+USE ClinicaMedica;
+GO
+
+CREATE USER clinicaUser FOR LOGIN clinicaUser;
+GO 
+
+ALTER ROLE db_datareader ADD MEMBER clinicaUser;
+ALTER ROLE db_datawriter ADD MEMBER clinicaUser;
+GO
+
+GRANT EXECUTE TO clinicaUser;
+GO 
+
+-- SI SE CREA ESTE USUARIO SE PUEDE CAMBIAR LA CADENA DE CONEXION A:
+--   "Server=SKINOFME;Database=ClinicaMedica;User Id=clinicaUser;Password=drugsxd;TrustServerCertificate=True;"
+--           (reemplazar)
