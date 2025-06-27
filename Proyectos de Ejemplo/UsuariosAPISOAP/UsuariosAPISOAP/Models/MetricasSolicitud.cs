de4d92a547cae34e2dcd1448aeed271cb8a242f4
PrisilaFloresDG <pflores@digitalgeko.com>
2025-06-27 11:52:14 -0600

    Ejemplos de ApiSOAP-Metricas-bcrypt

---
+++ Proyectos de Ejemplo/UsuariosAPISOAP/UsuariosAPISOAP/Models/MetricasSolicitud.cs
@@ -0,0 +1,17 @@
+﻿
+using System;
+namespace UsuariosAPISOAP.Models
+{
+    public class MetricasSolicitud
+    {
+        public int Id { get; set; }
+        public DateTime Fecha { get; set; }
+        public string Metodo { get; set; }
+        public string Ruta { get; set; }
+        public string Usuario { get; set; }
+        public long DuracionMilisegundos { get; set; }
+        public int CodigoRespuesta { get; set; }
+       
+
+    }
+}
