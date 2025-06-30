a48d44b9d4b1f90d95b8139eb38538ff2c1b2acf
Estiben-Hernandez <jhernandez@digitalgeko.com>
2025-06-30 17:47:39 -0600

    feat(AutoExpress_Entidades): Agreagar Modelo y DTO para tranajar con vehiculos

---
+++ Prueba1-Junior/APIS/Desarrollo/API SOAP/AutoExpress/AutoExpressSOAP/AutoExpress_Entidades/DTOs/VehiculoDTOs/VehiculoRequestDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AutoExpress_Entidades.DTOs.VehiculoDTOs
+{
+    public class VehiculoRequestDTO
+    {
+        public string Marca { get; set; }
+        public string Modelo { get; set; }
+        public int anio { get; set; }
+        public decimal Precio { get; set; }
+    }
+}
---
+++ Prueba1-Junior/APIS/Desarrollo/API SOAP/AutoExpress/AutoExpressSOAP/AutoExpress_Entidades/Models/Vehiculo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AutoExpress_Entidades.Models
+{
+    public class Vehiculo
+    {
+        public int Id { get; set; }
+        public string Marca { get; set; }
+        public string Modelo { get; set; }
+        public int anio { get; set; }
+        public decimal Precio { get; set; }
+        public bool Disponible { get; set; }
+    }
+}
