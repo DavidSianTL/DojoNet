73aaed665968d004499160ee17824f0c815acf60
Estiben-Hernandez <jhernandez@digitalgeko.com>
2025-07-14 06:04:45 -0600

    feat(MiBanco): Agregar proyecto API REST para gestión de clientes y pagos

---
+++ Prueba1-Junior/Evaluacion_Interna/APIREST/MiBanco/MiBanco/Data/DTOs/PagoDTOs/PagoRequestDTO.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MiBanco.Data.DTOs.PagoDTOs
+{
+	public class PagoRequestDTO
+	{
+		[Required]
+        [RegularExpression(@"^[1234567890]+$", ErrorMessage = "El campo {0} sólo puede contener nmeros.")]
+        public string? FK_IdCuentaOrigen { get; set; } = null;
+
+        [Required]
+        [RegularExpression(@"^[1234567890]+$", ErrorMessage = "El campo {0} sólo puede contener nmeros.")]
+        public string? FK_IdServicio { get; set; } = null;
+
+		[Required]
+        [RegularExpression(@"^[1234567890. ]+$", ErrorMessage = "El campo {0} sólo puede contener nmeros.")]
+        public decimal Monto { get; set; }
+		
+		[Required]
+        [RegularExpression(@"^[A-Za-zÁÉÍÓÚáéíóúÑñ 1234567890]+$", ErrorMessage = "El campo {0} sólo puede contener letras.")]
+        public string? Descripcion { get; set; } = null;
+
+    }
+
+}
---
+++ Prueba1-Junior/Evaluacion_Interna/APIREST/MiBanco/MiBanco/Data/DTOs/Response.cs
@@ -0,0 +1,16 @@
+﻿namespace MiBanco.Data.DTOs
+{
+	public class Response
+	{
+		public bool Exito { get; set; } = false;
+		public int Codigo { get; set; } = 500;
+		public string? Mensaje { get; set; } = "Error desconocido.";
+
+		public static Response Bueno(int codigo, string mensaje)
+			{ return new Response { Exito = true, Codigo = codigo, Mensaje = mensaje }; }		
+
+		public static Response Malo(int codigo, string mensaje)
+			{ return new Response { Exito = false, Codigo = codigo, Mensaje = mensaje }; }
+
+	}
+}
