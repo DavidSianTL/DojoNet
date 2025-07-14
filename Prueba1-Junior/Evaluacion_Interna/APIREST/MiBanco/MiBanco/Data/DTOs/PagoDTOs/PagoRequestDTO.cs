f9cb22ce4752b104239f8e7b8eb37fc5fcf33877
Estiben-Hernandez <jhernandez@digitalgeko.com>
2025-07-14 07:07:36 -0600

    feat: agregar archivo para prueba de consumo desde postman y validaciones adicionales

--- Prueba1-Junior/Evaluacion_Interna/APIREST/MiBanco/MiBanco/Data/DTOs/PagoDTOs/PagoRequestDTO.cs
+++ Prueba1-Junior/Evaluacion_Interna/APIREST/MiBanco/MiBanco/Data/DTOs/PagoDTOs/PagoRequestDTO.cs
@@ -6,10 +6,15 @@ namespace MiBanco.Data.DTOs.PagoDTOs
 	{
 		[Required]
         [RegularExpression(@"^[1234567890]+$", ErrorMessage = "El campo {0} sólo puede contener nmeros.")]
+        [MinLength(14, ErrorMessage = "El campo {0} sólo puede contener 14 dígitos.")]
+        [MaxLength(14, ErrorMessage = "El campo {0} sólo puede contener 14 dígitos.")]
+
         public string? FK_IdCuentaOrigen { get; set; } = null;
 
         [Required]
         [RegularExpression(@"^[1234567890]+$", ErrorMessage = "El campo {0} sólo puede contener nmeros.")]
+        [MinLength(14, ErrorMessage = "El campo {0} sólo puede contener 14 dígitos.")]
+        [MaxLength(14, ErrorMessage = "El campo {0} sólo puede contener 14 dígitos.")]
         public string? FK_IdServicio { get; set; } = null;
 
 		[Required]
@@ -18,6 +23,7 @@ namespace MiBanco.Data.DTOs.PagoDTOs
 		
 		[Required]
         [RegularExpression(@"^[A-Za-zÁÉÍÓÚáéíóúÑñ 1234567890]+$", ErrorMessage = "El campo {0} sólo puede contener letras.")]
+        [MaxLength(25, ErrorMessage = "El campo {0} no puede exceder 25 dígitos.")]
         public string? Descripcion { get; set; } = null;
 
     }
