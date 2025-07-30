f3a680892f4abff68ccb14e284c44fc523f74aba
Diego Catalan <dcatalan@digitalgeko.com>
2025-07-30 11:49:54 -0600

    dao ProyectoEquipo

---
+++ EquipoGeko/ProyectoDojoGeko/ProyectoDojoGeko/Models/ProyectoEquipo/EquipoViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace ProyectoDojoGeko.Models.ProyectoEquipo
+{
+    public class EquipoViewModel
+    {
+        public int IdEquipo { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public int FK_IdEstado { get; set; }
+    }
+}
---
+++ EquipoGeko/ProyectoDojoGeko/ProyectoDojoGeko/Models/ProyectoEquipo/ProyectoViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace ProyectoDojoGeko.Models.ProyectoEquipo
+{
+    public class ProyectoViewModel
+    {
+        public int IdProyecto { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public DateTime FechaInicio { get; set; }
+        public int FK_IdEstado { get; set; }
+    }
+}
