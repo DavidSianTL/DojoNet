c864560ff2b256e20bcf1daca3942ab6b3bef25f
Carlos <ccaal@digitalgeko.com>
2025-10-31 09:58:17 -0600

    Subiendo vistas pedidas por Diego

---
+++ ProyectoInventario/ProyectoInventario/Infrastructure/DbConnectionService.cs
@@ -0,0 +1,31 @@
+﻿using System.Data;
+using Microsoft.Data.SqlClient;
+
+
+namespace ProyectoInventario.Infrastructure
+{
+    public class DbConnectionService
+    {
+        private readonly string _connectionString;
+
+        public DbConnectionService(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public async Task<int> ExecuteNonQueryAsync(string spName, Dictionary<string, object> parameters)
+        {
+            using var connection = new SqlConnection(_connectionString);
+            using var command = new SqlCommand(spName, connection)
+            {
+                CommandType = CommandType.StoredProcedure
+            };
+
+            foreach (var param in parameters)
+                command.Parameters.AddWithValue(param.Key, param.Value ?? DBNull.Value);
+
+            await connection.OpenAsync();
+            return await command.ExecuteNonQueryAsync();
+        }
+    }
+}
